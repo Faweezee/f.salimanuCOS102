36e8505371364835acaa4482af005af4b22328db
Fawaz Salimanu <salimanufawaz@gmail.com>
2025-03-27 23:29:52 +0100

    completed week 2 projects

--- Week_2/project2.py
+++ Week_2/project2.py
@@ -3,6 +3,7 @@ print("\nTo calculate the root you will first have to select the type of equatio
 print("1. QUADRATIC\n2.CUBIC\n3. QUARTIC")
 opt = int(input("Please input the number associated with\nthe type of equation you'd like to calculate for :   "))
 if opt == 1:
+    print("You have chosen QUADRATIC EQUATION")
     a= float(input("WHAT IS A, THE COEFFICIENT OF X^2 :    "))
     b = float(input("WHAT IS B, THE COEFFICIENT OF X :   "))
     c = float(input("WHAT IS C, THE CONSTANT :    "))
@@ -19,4 +20,11 @@ if opt == 1:
     elif d < 0 :
         print("The equation has imaginary roots")
 elif opt == 2 :
-    print("")
+    print("You have chosen CUBIC EQUATION")
+    a = float(input("WHAT IS A, THE COEFFICIENT OF X^3 :    "))
+    b = float(input("WHAT IS B, THE COEFFICIENT OF X^2 :   "))
+    c = float(input("WHAT IS C, THE COEFFICIENT OF X :   "))
+    d = float(input("WHAT IS D, THE CONSTANT :    "))
+    print(f"THE EQUATION : {a}x^3 + {b}x^2 + {c}x + {d} ")
+elif opt == 3:
+    print("You have chosen QUARTIC EQUATION")
